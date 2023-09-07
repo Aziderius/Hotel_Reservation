8f0f618029dba9c3eeee1c78281a611d8956170b
Aziderius <143766082+Aziderius@users.noreply.github.com>
2023-09-06 18:36:55 -0700

    Commit inicial del código de control de reservaciones del Hotel Hazzbin

---
+++ Hazzbin Hotel.py
@@ -0,0 +1,45 @@
+import datetime
+
+#Habitaciones disponibles del Hotel Hazzbin"
+rooms = {
+    "floor_1": {"single1": True, "single2": True, "single3": True, "single4": True, "single5": True},
+    "floor_2": {"single6": True, "single7": True, "single8": True, "single9": True, "single10": True},
+    "floor_3": {"doble1": True, "doble2": True, "doble3": True, "doble4": True, "doble5": True},
+    "floor_4": {"doble6": True, "doble7": True, "doble8": True, "doble9": True, "doble10": True},
+    "floor_5": {"luxury1": True, "luxury2": True, "luxury3": True, "luxury4": True, "luxury5": True}
+}
+
+class Client:
+    def __init__(self, name, phonenum, email):
+        self.name = name
+        self.phonenum = phonenum
+        self.email = email
+
+class Reservation(Client):
+    def __init__(self, initial_date, final_date, type_room):
+        self.initial_date = initial_date
+        self.final_date = final_date
+        self.type_room = type_room
+
+def register_reservation():
+    print("Welcome to Hazzbin Hotel reservation system,")
+    print("please fill the fields correctly.")
+
+    name = input("Full name: ")
+    phonenum = input("Phone Number: ")
+    email = input("Email: ")
+
+    initial_date = input("Initial reservation date (YYYY-MM-DD): ")
+    try:
+        initial_date = datetime.datetime.strptime(initial_date, '%Y-%m-%d').date()
+    except ValueError:
+        return False, "Invalid Input. Please use 'YYYY-MM-DD' format."
+     
+    final_date = input("Final reservation date (YYYY-MM-DD): ")
+    try:
+        final_date = datetime.datetime.strptime(final_date, '%Y-%m-%d').date()
+    except ValueError:
+        return False, "Invalid Input. Please use 'YYYY-MM-DD' format."
+    
+    type_room = input("Type of room (single, doble, luxury): ")
+    
